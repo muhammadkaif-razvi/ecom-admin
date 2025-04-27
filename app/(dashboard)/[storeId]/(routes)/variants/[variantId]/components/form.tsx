39da422dae77e2d51ffdb1e7f077a47bcd91f9ab
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-04-27 23:16:11 +0530

    fix: limit maximum file uploads to 3 in VariantForm component

--- app/(dashboard)/[storeId]/(routes)/variants/[variantId]/components/form.tsx
+++ app/(dashboard)/[storeId]/(routes)/variants/[variantId]/components/form.tsx
@@ -181,7 +181,7 @@ export const VariantForm: React.FC<variantFormProps> = ({
                       );
                       field.onChange(updatedValue);
                     }}
-                    maxFiles={4}
+                    maxFiles={3}
                   />
                 </FormControl>
                 <FormMessage />
