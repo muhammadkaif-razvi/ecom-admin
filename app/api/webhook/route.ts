36c55312d42308b04f6c29d1da0be390cc0889c6
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-05-11 14:28:46 +0530

    fix: move Stripe initialization to webhook route and remove unused stripe.ts file

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,9 +3,10 @@ import { headers } from "next/headers";
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
-const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
-  apiVersion: "2025-03-31.basil",
-});
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion:"2025-03-31.basil",
+  typescript: true
+})
 
 export async function POST(req: Request) {
   const body = await req.text();
