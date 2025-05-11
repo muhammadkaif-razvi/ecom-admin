91fa4cf173eb1b64015b6473380568450cb4a08b
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-05-11 20:41:09 +0530

    fix: uncomment Stripe import for consistency in webhook route

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,8 +2,14 @@ import { db } from "@/lib/db";
 import { headers } from "next/headers";
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
-import { stripe } from "@/lib/stripe";
+// import { stripe } from "@/lib/stripe";
 
+
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion:"2025-03-31.basil",
+  typescript: true
+})
 
 export async function POST(req: Request) {
   const body = await req.text();
