6e1c038bc5fa652052be782c831192104ffa7ab4
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-05-11 20:38:13 +0530

    fix: move Stripe instance to a separate file for better modularity

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,11 +2,8 @@ import { db } from "@/lib/db";
 import { headers } from "next/headers";
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
+import { stripe } from "@/lib/stripe";
 
- export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion:"2025-03-31.basil",
-  typescript: true
-})
 
 export async function POST(req: Request) {
   const body = await req.text();
