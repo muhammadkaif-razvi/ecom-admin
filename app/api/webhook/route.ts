ecc3f97fdf7ae895966daa8e5e83d9a68f9f21f6
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-05-11 21:02:01 +0530

    fix: remove unused Stripe instance file and update import in webhook route

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,14 +2,11 @@ import { db } from "@/lib/db";
 import { headers } from "next/headers";
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
-// import { stripe } from "@/lib/stripe";
 
-
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion:"2025-03-31.basil",
-  typescript: true
-})
+const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+  apiVersion: "2025-03-31.basil",
+  typescript: true,
+});
 
 export async function POST(req: Request) {
   const body = await req.text();
